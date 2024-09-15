5f66b34c367a43c43f32eea86af9801fd60023d9
Vijeth-Rai <vijethrai98@gmail.com>
2024-09-15 19:00:45 +0530

    Environment variables config file

---
+++ configs/ConfigEnv.py
@@ -0,0 +1,19 @@
+from utils.imports import *
+
+load_dotenv()
+
+groq_api_key = os.getenv("GROQ")
+llm = ChatGroq(
+    groq_api_key=groq_api_key,
+    model_name="llama3-groq-8b-8192-tool-use-preview"
+)
+
+# Initialize MongoDB saver
+mongo_uri = os.getenv("MONGO_URI")
+database_name = os.getenv("DATABASE_NAME")
+collection_name = os.getenv("COLLECTION_NAME")
+
+client = MongoClient(mongo_uri)
+db = client[database_name]
+collection = db[collection_name]
+
